ff2f55fd51d81fb5439151ec2d0975aff6d0d511
geethakammili <2200090071@kluniversity.in>
2024-01-11 14:18:14 +0530

    firstcommit

---
+++ app.py
@@ -0,0 +1,8 @@
+This is a Python Programint sum1,sum2,sum3;
+scanf &sum1,&sum2;printf enter the values;
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[This is a Python Program
+int sum1,sum2,sum3;
+scanf &sum1,&sum2;
+printf enter the values;
+sum3=sum1+sum2;
+printf sum3
